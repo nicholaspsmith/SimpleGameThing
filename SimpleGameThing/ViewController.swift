583e661944da82ab5cffb9aa68052887f3138fb0
Nick P Smith <npsmith1990@gmail.com>
2015-11-25 17:28:34 -0700

    Add ability to attack each other

--- SimpleGameThing/ViewController.swift
+++ SimpleGameThing/ViewController.swift
@@ -10,17 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    let game = Game()
-    game.run()
-  }
+    @IBOutlet weak var knightHPLabel: UILabel!
+    @IBOutlet weak var ogreHPLabel: UILabel!
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var knightImage: UIImageView!
+    @IBOutlet weak var ogreImage: UIImageView!
+    
+    var knight: Character!
+    var ogre: Character!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
+        knight = Character(name: "Avatar", startingHp: 120, startingAttackPower: 20)
+        ogreHPLabel.text = "\(knight.hp) HP"
+
+        ogre = Character(name: "Avatar", startingHp: 120, startingAttackPower: 20)
+        knightHPLabel.text = "\(ogre.hp) HP"
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 
 
+    @IBAction func pressOgreAttack(sender: AnyObject) {
+        if knight.attemptAttack(ogre.attackPower) {
+            mainLabel.text = "Attacked knight for \(ogre.attackPower) HP"
+            knightHPLabel.text = "\(knight.hp) HP"
+        } else {
+            mainLabel.text = "Attack was unsuccessful"
+        }
+        
+        if !knight.isAlive {
+            knightHPLabel.text = ""
+            mainLabel.text = "Killed knight"
+            knightImage.hidden = true
+        }
+    }
+    
+    @IBAction func pressKnightAttack(sender: AnyObject) {
+        if ogre.attemptAttack(knight.attackPower) {
+            mainLabel.text = "Attacked ogre for \(knight.attackPower) HP"
+            ogreHPLabel.text = "\(ogre.hp) HP"
+        } else {
+            mainLabel.text = "Attack was unsuccessful"
+        }
+        
+        if !ogre.isAlive {
+            ogreHPLabel.text = ""
+            mainLabel.text = "Killed ogre"
+            ogreImage.hidden = true
+        }
+    }
 }
 
